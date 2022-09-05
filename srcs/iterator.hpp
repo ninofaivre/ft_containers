0194b92314b70eac1f20278205448ed2f4d0ccdd
nino <nino.faivre@netc.fr>
2022-09-05 11:06:38 +0200

    updated iterator_traits for const T * -> non const value_type but const pointer and reference

--- srcs/iterator.hpp
+++ srcs/iterator.hpp
@@ -37,13 +37,20 @@ namespace ft
 	{
 		typedef std::ptrdiff_t	difference_type;
 		typedef T				value_type;
-		typedef T*				pointer;
-		typedef T&				reference;
+		typedef value_type*		pointer;
+		typedef value_type&		reference;
 		typedef ft::random_access_iterator_tag	iterator_category;
 	};
 
 	template<class T>
-	struct iterator_traits<const T *> : iterator_traits<T *> {};
+	struct iterator_traits<const T *>
+	{
+		typedef std::ptrdiff_t	difference_type;
+		typedef T				value_type;
+		typedef const T*		pointer;
+		typedef const T&		reference;
+		typedef ft::random_access_iterator_tag	iterator_category;
+	};
 
 	template<class InputIt>
 	typename ft::iterator_traits<InputIt>::difference_type	distance(InputIt first,
