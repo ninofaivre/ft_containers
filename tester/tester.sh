10f39058cfec7ece8019a40fc9c5b9c7b4534c9f
nino <nino.faivre@netc.fr>
2022-09-29 01:27:14 +0200

    tester minor improvement

--- tester/tester.sh
+++ tester/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # utils #
 C_RESET="\e[0m"
-C_PURPLE="\e[94m"
+C_PURPLE="\e[95m"
 C_RED="\e[91m"
 C_GREEN="\e[92m"
 C_YELLOW="\e[93m"
@@ -9,6 +9,7 @@ C_YELLOW="\e[93m"
 S_RESET="\033[0m"
 S_BOLD="\033[1m"
 
+SEP="${S_BOLD}|${S_RESET}"
 OK="${S_BOLD}${C_GREEN}\U2713${C_RESET}${S_RESET}"
 MOK="${S_BOLD}${C_YELLOW}\U2713${C_RESET}${S_RESET}"
 KO="${C_RED}\U2620${C_RESET}"
@@ -70,18 +71,18 @@ fi
 # parse #
 function	compil()
 {
-	make softClean namespace="$1" >/dev/null 2>&1
-	echo -n $(echo "$1" | awk '{ print toupper($0) }')
+	make softClean namespace="$1" &>/dev/null
+	echo -n ${1^^}
 	if [ "$1" = "ft" ]; then echo -n " "; fi
-	if ! t=$( { time timeout "$CCTimeOut" make ${makeArgs[@]} namespace="$1"; } 2>&1); then
-		if echo "$t" | grep -c "Terminated" >/dev/null; then
+	if ! t=$( { time timeout "$CCTimeOut" make ${makeArgs[@]} namespace="$1" &>/dev/null; } 2>&1); then
+		if (echo "$t" | grep -c "Terminated" >/dev/null); then
 			echo -e " Make ${C_RED}failed${C_RESET} !(Timed out)"
 		else
 			echo -e " Make ${C_RED}failed${C_RESET} !"
 		fi
 		exit 1
 	fi
-	t=$(echo "$t" | tail -n 3 | awk 'NR==1 { print $2 }')
+	t=$(echo "$t" | awk 'NR==2 { print $2 }')
 	echo -e " Make ${C_GREEN}succeeded${C_RESET}" "	time :" "$t"
 }
 
@@ -92,21 +93,21 @@ function pres() #subtestname #F[rawData] #S[rawData] #only_comp
 	if [ "$4" != "compact" ]; then
 		echo -e -n " F[$2] S[$3]"
 	fi
-	echo -n "|"
+	echo -e -n "${SEP}"
 }
 
 function presTime()
 {
 	#echo -n "$1|$2"
-	stdTime=$(echo $1 | awk -F'[.ms]' '{ printf("%d"), $3 }')
-	ftTime=$(echo $2 | awk -F'[.ms]' '{ printf("%d"), $3 }')
+	stdTime=$(awk -F'[.ms]' '{ printf("%d"), $3 }' <<< "$1")
+	ftTime=$(awk -F'[.ms]' '{ printf("%d"), $3 }' <<< "$2")
 	diff=$((ftTime-stdTime))
 	if ((diff < 0)); then
 		note=100
 	elif ((diff < 5)); then
 		note=100
 	else
-		note=$(echo | awk -v ft=$ftTime -v std=$stdTime '{ printf("%3d"), 100-(ft/std*5) }')
+		note=$(awk -v ft=$ftTime -v std=$stdTime '{ printf("%3d"), 100-(ft/std*5) }' <<< "")
 		if ((note < 0)) ; then note="  0"; fi
 	fi
 	echo -n "perf"
@@ -117,7 +118,7 @@ function presTime()
 	else
 		echo -n -e "${OK}${C_GREEN}"
 	fi
-	echo -n -e "$note${C_RESET}/100|"
+	echo -n -e " $note${C_RESET}/100${SEP}"
 }
 
 TESTS=()
@@ -134,20 +135,20 @@ FT_TIMES=()
 
 function	OneTest()
 {
-	if [ "$(test -f ./.exec/std_$2/$1 && echo Y || echo N)" = "N" ]; then
+	if !(test -f ./.exec/std_$2/$1); then
 		STD_PIDS+=("none")
 		STD_COMPILS+=("N")
 	else
 		STD_COMPILS+=("Y")
-		({ time timeout "$ExecTimeOut" ./.exec/std_"$2"/"$1" >./outputs/std_"$2"/"$1".output 2>&1; } > ./outputs/std_"$2"/"$1".time 2>&1) &
+		({ time timeout "$ExecTimeOut" ./.exec/std_"$2"/"$1" &>./outputs/std_"$2"/"$1".output; } 2>./outputs/std_"$2"/"$1".time) &
 		STD_PIDS+=("$!")
 	fi
-	if [ "$(test -f ./.exec/ft_$2/$1 && echo Y || echo N)" = "N" ]; then
+	if !(test -f ./.exec/ft_$2/$1) ; then
 		FT_PIDS+=("none")
 		FT_COMPILS+=("N")
 	else
 		FT_COMPILS+=("Y")
-		({ time timeout "$ExecTimeOut" ./.exec/ft_"$2"/"$1" >./outputs/ft_"$2"/"$1".output 2>&1; } > ./outputs/ft_"$2"/"$1".time 2>&1) &
+		({ time timeout "$ExecTimeOut" ./.exec/ft_"$2"/"$1" &>./outputs/ft_"$2"/"$1".output; } 2>./outputs/ft_"$2"/"$1".time) &
 		FT_PIDS+=("$!")
 	fi
 }
@@ -194,29 +195,31 @@ function	exe()
 function	displayTest() #testID #ContainerName
 {
 	if ([ "${FT_COMPILS[${1}]}" = "Y" ] && [ "${STD_COMPILS[${1}]}" = "Y" ]); then
-		sameOutput=$(diff -q ./outputs/std_"$2"/"${TESTS[${1}]}".output ./outputs/ft_"$2"/"${TESTS[${1}]}".output >/dev/null && echo 1 || echo 0)
+		cmp ./outputs/std_"$2"/"${TESTS[${1}]}".output ./outputs/ft_"$2"/"${TESTS[${1}]}".output &>/dev/null; diffOutput=$?
 	fi
-	
-	echo -n "$(echo | awk -v testName=${TESTS[${1}]} -v maxTestNameLength=$maxTestNameLength -v testNameLength=${#TESTS[${1}]} '{ printf("%s%"maxTestNameLength-testNameLength"s\n", substr(testName, 1, maxTestNameLength), "") ; }')""|"
+	awk -v name="${TESTS[${1}]}" -v maxNameLen="$maxTestNameLength"\
+		'{ nameLen = length(name) ; te = nameLen <= maxNameLen;
+		printf("%s%"te * (maxNameLen - nameLen)"s%s", substr(name, 1, maxNameLen - !te), "", substr(". ", 1 + te, 2)); }' <<< ""
+	echo -n -e "$SEP"
 	pres "CC" "${FT_COMPILS[${1}]}" "${STD_COMPILS[${1}]}" "full"
 	if [ "${STD_RETURNS[${1}]}" = "124" ]; then
-		echo -e "${C_RED}STD Timed Out${C_RESET}|"
+		echo -e "${C_RED}STD Timed Out${C_RESET}"
 		return ;
 	elif [ "${FT_RETURNS[${1}]}" = "124" ]; then
-		echo -e "${C_RED}FT Timed Out ${C_RESET}|"
+		echo -e "${C_RED}FT Timed Out ${C_RESET}"
 		return ;
 	fi
 	if ([ "${FT_COMPILS[${1}]}" = "N" ] && [ "${STD_COMPILS[${1}]}" = "Y" ]); then
-		echo -e "${C_RED} FT shall CC ${C_RESET}|"
+		echo -e "${C_RED} FT shall CC ${C_RESET}"
 		return ;
 	elif ([ "${FT_COMPILS[${1}]}" = "Y" ] && [ "${STD_COMPILS[${1}]}" = "N" ]); then
-		echo -e "${C_RED}FT shallnt CC${C_RESET}|"
+		echo -e "${C_RED}FT shallnt CC${C_RESET}"
 		return ;
 	elif ([ "${FT_COMPILS[${1}]}" = "N" ] && [ "${STD_COMPILS[${1}]}" = "N" ]); then
-		echo -e "${C_GREEN}CC fail test ${C_RESET}|"
+		echo -e "${C_GREEN}CC fail test${C_RESET}"
 		return ;
 	fi
-	pres "Out" "1" "$sameOutput" "compact"
+	pres "Out" "0" "$diffOutput" "compact"
 	pres "Ret" "${FT_RETURNS[${1}]}" "${STD_RETURNS[${1}]}" "compact"
 	presTime "${STD_TIMES[${1}]}" "${FT_TIMES[${1}]}"
 	echo
@@ -231,7 +234,7 @@ function	oneContainer()
 	fi
 	exe "$1"
 	echo
-	echo "TEST OF" $(echo | awk "{ print toupper(\"$1\") }") ":"
+	echo -e "TEST OF" "${C_PURPLE}${1^^}${C_RESET}" ":"
 	echo
 	for i in ${!TESTS[@]}; do
 		displayTest "$i" "$1"
